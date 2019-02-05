8463b0f9467e0bf4789978c348e0ce6a81482dd9
nlthevinh <46483319+nlthevinh@users.noreply.github.com>
2019-02-05 11:53:41 +0100

    Create list.php

---
+++ list.php
@@ -0,0 +1,44 @@
+<?php
+//Info base
+$dbhost = "localhost";
+$dbuser = "rt";
+$dbpass = "rtlry";
+$db = "test";
+$user="";
+$Liste="";
+$Message="";
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname='.$db.';charset=utf8', $dbuser, $dbpass);
+}
+catch (Exception $e)
+{
+		die('Erreur : ' . $e->getMessage());
+}
+if (isset($_GET["user"])){
+    $user = $_GET["user"];
+    $sql = "SELECT * FROM messages WHERE destinataire='".$user."'";
+    $reponse = $bdd->query($sql);
+    echo "<ul>";
+	while ($donnees = $reponse->fetch()) {
+        
+		$point_fin=""; 
+		$apercu = substr($donnees['message'], 0, 10);
+		if(strlen($donnees['message'])>10){
+			$point_fin="...";
+		}
+        
+		echo "<li class=\"liste_mail\" onclick=\"afficherMail(".$donnees['id'].")\">
+							<a id=\"listeMail\"  href=\"#l\">
+								".$donnees['date']." <b>".$donnees['expediteur']."</b> : ".$apercu."".$point_fin."
+							</a>
+							<a id=\"croix\" onclick=\"supprimer(".$donnees['id'].",'".$user."')\" href=\"#\">
+								<span class=\"croixgauche\"></span>
+                                <span class=\"croixdroite\"></span>
+							</a>
+						</li>";
+    
+	}
+ echo "</ul>";
+}
+?>
